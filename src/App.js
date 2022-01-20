2a4c2a3658da0d7230ebc740c14d80413400174f
Samir Gayatri <samirgupta19670@gmail.com>
2022-01-20 15:18:53 +0530

    Samir

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React from 'react';
 
-import{ Cards, Chart, CountryPicker } from './components'
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
 import styles from './App.module.css';
-import { fetchData } from './api';
 
-class App extends React.Component{
-    state = {
-       data: {}, 
-    }
+import image from './images/image.png';
 
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
-    async componentDidMount(){
-        const fetchedData = await fetchData();
+  async componentDidMount() {
+    const data = await fetchData();
 
-        this.setState({ data: fetchedData});
-    }
+    this.setState({ data });
+  }
 
-    render(){
-        const { data } = this.state;
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
 
-        return(
-            <div className={styles.container}>
-                <Cards data={data} />
-                <CountryPicker />
-                <Chart />
-            </div>
-        )
-    }
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/api/index.js
+++ src/api/index.js
@@ -2,27 +2,49 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
-    try{
-        const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+export const fetchData = async (country) => {
+  let changeableUrl = url;
 
-        return { confirmed, recovered, deaths, lastUpdate };
-    } catch (error) {
-    }
-}
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
 
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
+
+// export const fetchDailyData = async () => {
+//   try {
+//     const { data } = await axios.get(`${url}/daily`);
+
+//     return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+//   } catch (error) {
+//     return error;
+//   }
+// };
+
+// Instead of Global, it fetches the daily data for the US
 export const fetchDailyData = async () => {
- try{
-   const { data } = await axios.get('${url}/daily');
+    try {
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+  
+      return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+    } catch (error) {
+      return error;
+    }
+  };
 
-   const modifiedData = data.map((dailyData) => ({
-    confirmed: dailyData.confirmed.total,
-    deaths: dailyData.deaths.total,
-    date: dailyData.reportDate,
-   }));
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
 
-   return modifiedData;
- } catch (error) {
-
- }
-}
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
